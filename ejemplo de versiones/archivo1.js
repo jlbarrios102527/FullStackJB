ab73b4fb60d55bc69e6d20ceed116bf5b21f16b6
Jorge Barrios <jorgeluisbarrios0625@gmail.com>
2025-04-02 10:36:02 -0500

    CAMBIO 4

--- ejemplo de versiones/archivo1.js
+++ ejemplo de versiones/archivo1.js
@@ -1,3 +1,4 @@
 console.log("esto es un mensaje");
 console.log("cambio 1");
 console.log("cambio 2");
+console.log("cambio 3");
--- ejemplo de versiones/archivo3.js
+++ ejemplo de versiones/archivo3.js
@@ -1,3 +1,4 @@
 console.log("cambio 3");
 console.log("cambio 1");
 console.log("cambio 2");
+console.log("cambio 4");
